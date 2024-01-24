729be99c6970162c5cf19878c3574097bdddd931
Giancarlo Ricci <gricci@stanford.edu>
2024-01-24 23:55:06 +0000

    wrapped integers passing tests

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,24 @@
 #include "wrapping_integers.hh"
 
 using namespace std;
+#define OFFSET ( 1UL << 32 ) // 2 ^ 32
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32 { zero_point + uint32_t( n ) };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  // STEP 1: Convert to wrapped representation
+  uint32_t wrapped = wrap( checkpoint, zero_point ).raw_value_;
+
+  // STEP 2: Since wrapped will truncate, this represents the meaningful byte offset
+  int32_t offset = raw_value_ - wrapped;
+
+  // STEP 3: Find absolute sequence using offset
+  int64_t abs_seq = checkpoint + offset;
+
+  // STEP 4: Account for overflow
+  return abs_seq < 0 ? abs_seq + OFFSET : abs_seq;
 }
