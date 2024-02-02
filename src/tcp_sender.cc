c3a954a6774ad6cda7b6ee8eec01a4cbc587a644
Giancarlo Ricci <gricci@stanford.edu>
2024-02-02 01:29:38 +0000

    passing more tests

--- src/tcp_sender.cc
+++ src/tcp_sender.cc
@@ -1,6 +1,7 @@
 #include "tcp_sender.hh"
 #include "tcp_config.hh"
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -8,7 +9,9 @@ Timer::Timer( uint64_t init_RTO ) : time( init_RTO ), initial_RTO( init_RTO ), R
 
 void Timer::elapse( uint64_t time_elapsed )
 {
-  time -= time_elapsed;
+  if ( running ) {
+    time -= std::min( time, time_elapsed );
+  }
 }
 
 bool Timer::expired()
@@ -55,7 +58,7 @@ void TCPSender::push( const TransmitFunction& transmit )
 {
 
   // special case for initial push
-  if (bytes_pushed_ == 0) {
+  if ( bytes_pushed_ == 0 ) {
     TCPSenderMessage msg = make_empty_message();
     transmit( msg );
     bytes_pushed_ += msg.sequence_length();
@@ -66,13 +69,14 @@ void TCPSender::push( const TransmitFunction& transmit )
 
   while ( input_.reader().bytes_buffered() > 0 && capacity > bytes_pushed_ ) {
 
-    uint64_t msg_len = std::min( input_.reader().bytes_buffered(), TCPConfig::MAX_PAYLOAD_SIZE );
-    std::string_view buffer = input_.reader().peek().substr( 0, msg_len );
+    uint64_t msg_len
+      = std::min( { capacity - bytes_pushed_, input_.reader().bytes_buffered(), TCPConfig::MAX_PAYLOAD_SIZE } );
+    std::string buffer = std::string( input_.reader().peek().substr( 0, msg_len ) );
     input_.reader().pop( msg_len );
 
     TCPSenderMessage msg;
     msg.seqno = Wrap32::wrap( bytes_pushed_, isn_ );
-    msg.SYN = bytes_pushed_ == 0;
+    msg.SYN = false;
     msg.payload = buffer;
     msg.FIN = input_.reader().is_finished();
 
@@ -86,8 +90,8 @@ TCPSenderMessage TCPSender::make_empty_message() const
 {
   TCPSenderMessage message;
   message.seqno = Wrap32::wrap( bytes_pushed_, isn_ );
-  message.SYN = bytes_pushed_ == 0;
-  message.payload = "";
+  message.SYN = ( bytes_pushed_ == 0 );
+  message.payload = {};
   message.FIN = false;
   message.RST = false;
   return message;
@@ -98,20 +102,15 @@ void TCPSender::receive( const TCPReceiverMessage& msg )
   window_size_ = msg.window_size;
 
   if ( msg.ackno.has_value() ) {
-    uint64_t recieved_ackno = msg.ackno.value().unwrap( isn_, ack_no_ );
+    uint64_t received_ackno = msg.ackno.value().unwrap( isn_, ack_no_ );
 
     while ( !outstanding_segments_.empty() ) {
 
       TCPSenderMessage front_msg = outstanding_segments_.front();
-      ack_no_ += front_msg.sequence_length();
-      uint64_t front_msg_seq_no = front_msg.seqno.unwrap( isn_, ack_no_ );
-
-      // if this outstanging segment has been acknowledged, remove it
-      if ( front_msg_seq_no + front_msg.sequence_length() <= recieved_ackno ) {
-        outstanding_segments_.pop();
-      } else {
+      if ( received_ackno < ack_no_ + front_msg.sequence_length() )
         break;
-      }
+      ack_no_ += front_msg.sequence_length();
+      outstanding_segments_.pop();
     }
 
     if ( outstanding_segments_.empty() )
