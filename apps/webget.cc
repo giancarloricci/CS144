bd9f450f2ebe81a1a4173afd66acceb2414e42b7
Giancarlo Ricci <gricci@stanford.edu>
2024-02-26 02:59:28 +0000

    checkpoint 4 webget change

--- apps/webget.cc
+++ apps/webget.cc
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <span>
 #include <string>
+#include "tcp_minnow_socket.hh"
 
 using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  TCPSocket socket;
+  CS144TCPSocket socket;
   socket.connect( Address( host, "http" ) );
   socket.write( "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n" );
   while ( !socket.eof() ) {
